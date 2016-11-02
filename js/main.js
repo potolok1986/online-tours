840883748d9b51c6e0de8fb6a2e1ddd0ea8bbedd
al.v.kuzmin <al.v.kuzmin@s7.ru>
2016-11-02 15:10:38 +0700

    выпадающее меню и скролбар

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(function () {
-    $("select").select2({
-        minimumResultsForSearch: -1,
-        dropdownAutoWidth : true,
-        width: 'auto'
-    });
-    $(".select2-results__options").perfectScrollbar()
+	$("select").select2({
+		minimumResultsForSearch: -1,
+		width: 'auto'
+	})
+		.on("select2:opening",function () {
+			// обновляем
+			updateScrollBar(true)
+		})
+		.on("select2:open", function () {
+			// инициализируем
+			updateScrollBar()
+	});
+	function updateScrollBar(update){
+		// Вещаем таймаут, т.к. список может не успеть построится до конца, и скролл срау не появится
+		setTimeout(function () {
+			$(".select2-results__options").perfectScrollbar(update ? "update" : null)
+		},10);
+	}
 });
