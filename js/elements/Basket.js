fe7fc185402893e3e653ab5237c5d8bb7aa6657f
Александр <fnpal@mail.ru>
2016-11-04 20:16:44 +0600

    финальные правки

--- js/elements/Basket.js
+++ js/elements/Basket.js
@@ -1,49 +1,55 @@
-var
+// создаем класс Basket
+var Basket = (function () {
+	var
+		i,// counters
+
 // Приватные свойства
-	__sum,
-	__countryCounter,
-	__tourInBasket = [],
+		__sum,
+		__countryCounter,
+		__toursInBasket = [],
 
 // Конструктор класса
-	Basket = function (selector) {
-		this.selector = $(selector);
-		this.selector.empty().append(
-			"<p>В корзине <span class='js-basket-info-counter'></span> на сумму <span class='js-basket-info-sum'></span></p>"
-		);
-		this.sum = this.selector.find(".js-basket-info-sum");
-		this.tourCounter = this.selector.find(".js-basket-info-counter");
-		this.update();
-	};
-// Публичные методы
-Basket.prototype.update = function (tour) {
-	__sum = __countryCounter = 0;
-	if (tour) {
-		__tourInBasket.push(tour);
-	}
-	for (var i in __tourInBasket) {
-		__sum += __tourInBasket[i].price;
-		__countryCounter++;
-	}
-
-	this.sum.text(__sum + " рубл".pluralize(__sum,"ь","я","ей") + ".");
-	this.tourCounter.text(__countryCounter + " товар".pluralize(__countryCounter,"","а","ов"));
-};
-Basket.prototype.add = function (tour) {
-	var ok = true; // флаг на поиск совпадений по странам
-	for (var i in __tourInBasket) {
-		if (__tourInBasket[i].country.indexOf(tour.country) != -1) {
-			ok = false;
-			break;
-		}
-	}
-	ok ? this.update(tour) : alert("Тур в " + tour.country + " уже есть в Вашей корзине");
-};
-Basket.prototype.remove = function (tour) {
-	for(var i in __tourInBasket){
-		if(__tourInBasket[i].id === tour.id){
-			delete  __tourInBasket[i];
+		Basket = function (selector) {
+			this.selector = $(selector);
+			this.selector.empty().append(
+				"<p>В корзине <span class='js-basket-info-counter'></span> на сумму <span class='js-basket-info-sum'></span></p>"
+			);
+			this.sum = this.selector.find(".js-basket-info-sum");
+			this.tourCounter = this.selector.find(".js-basket-info-counter");
 			this.update();
-			break;
+		};
+// Публичные методы
+	Basket.prototype.update = function (tour) {
+		__sum = __countryCounter = 0;
+		if (tour) {
+			__toursInBasket.push(tour);
 		}
-	}
-};
+		for (i in __toursInBasket) {
+			__sum += __toursInBasket[i].price;
+			__countryCounter++;
+		}
+
+		this.sum.text(__sum + " рубл".pluralize(__sum,"ь","я","ей") + ".");
+		this.tourCounter.text(__countryCounter + " товар".pluralize(__countryCounter,"","а","ов"));
+	};
+	Basket.prototype.add = function (tour) {
+		var ok = true; // флаг на поиск совпадений по странам
+		for (i in __toursInBasket) {
+			if (__toursInBasket[i].country.indexOf(tour.country) != -1) {
+				ok = false;
+				break;
+			}
+		}
+		ok ? this.update(tour) : alert("Тур в " + tour.country + " уже есть в Вашей корзине");
+	};
+	Basket.prototype.remove = function (tour) {
+		for(i in __toursInBasket){
+			if(__toursInBasket[i].id === tour.id){
+				delete  __toursInBasket[i];
+				this.update();
+				break;
+			}
+		}
+	};
+	return Basket
+}());
